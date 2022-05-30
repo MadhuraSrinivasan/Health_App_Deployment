12f929c9b7f93e24a0e665e866e2096ae9335406
MadhuraSrinivasan <93370037+MadhuraSrinivasan@users.noreply.github.com>
2022-05-30 21:42:08 +0530

    Add files via upload

---
+++ Saving_ML_Models/pickle/heart_load_model.py
@@ -0,0 +1,26 @@
+
+import pickle
+import numpy as np 
+import pandas as pd 
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+
+url = "https://raw.githubusercontent.com/MadhuraSrinivasan/ML-Projects/main/heart.csv"
+names = ['age', 'sex', 'cp', 'trestbps', 'chol', 'fbs', 'restecg', 'thalach','exang', 'oldpeak', 'slope', 'ca', 'thal', 'target']
+heart_df = pd.read_csv(url,names=names)
+
+#print(heart_df)
+x1 = heart_df.iloc[::,0:len(heart_df.columns)-1]
+#print(x)
+y1= heart_df['target']
+
+x_train,x_test,y_train,y_test = train_test_split(x1,y1,test_size=0.3)
+
+model1 = RandomForestClassifier(min_samples_split=3,n_estimators=200,n_jobs=-1)
+model1.fit(x_train,y_train)
+
+
+filename1 = 'heart_model.sav'
+heart_model = pickle.load(open(filename1,'rb'))
+heart_result = model1.score(x_test,y_test)
+
